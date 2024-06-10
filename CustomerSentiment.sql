dedddef211ce212e050fcf9bc201a19b9ad812e0
Pallavi Vishnu <165426777+pallavi-vishnu@users.noreply.github.com>
2024-06-09 20:05:03 -0700

    Add files via upload

---
+++ CustomerSentiment.sql
@@ -0,0 +1,43 @@
+-- Let's take a look at the table
+SELECT *
+FROM [CustomerSentiments].[dbo].[Call-Center-Sentiment-Sample-Data]
+
+
+
+-- Create a column which shows the day of the week on which the calls were made
+SELECT ID, Customer_Name, Sentiment, Call_Timestamp, Reason, City, State, Channel, Response_Time, Call_Duration_Minutes, Call_Center,DATENAME(DW, Call_Timestamp) AS DayOfWeek
+FROM [CustomerSentiments].[dbo].[Call-Center-Sentiment-Sample-Data]
+
+-- A temp table with the day of the week column
+CREATE TABLE #CallsByDay
+(ID nvarchar(50) NOT NULL,                      
+Customer_Name nvarchar(50) NOT NULL,
+Sentiment nvarchar(50) NOT NULL,
+Call_Timestamp date NOT NULL,
+Reason nvarchar(50) NOT NULL,
+City nvarchar(50) NOT NULL,
+State nvarchar(50) NOT NULL,
+Channel nvarchar(50) NOT NULL,
+Response_Time nvarchar(50) NOT NULL,
+Call_Duration_Minutes tinyint NOT NULL,
+Call_Center nvarchar(50) NOT NULL,
+DayOfWeek nvarchar(50)
+)
+
+INSERT INTO #CallsByDay
+SELECT ID, Customer_Name, Sentiment, Call_Timestamp, Reason, City, State, Channel, Response_Time, Call_Duration_Minutes, Call_Center, DATENAME(DW, Call_Timestamp) AS DayOfWeek
+FROM [CustomerSentiments].[dbo].[Call-Center-Sentiment-Sample-Data]
+
+
+-- Has any customer called more than once?
+SELECT
+    Customer_Name, COUNT(Customer_Name)
+FROM
+    [CustomerSentiments].[dbo].[Call-Center-Sentiment-Sample-Data]
+GROUP BY
+    Customer_Name
+HAVING 
+    COUNT(Customer_Name) > 1
+
+
+-- What is the most common sentiment of the callers
